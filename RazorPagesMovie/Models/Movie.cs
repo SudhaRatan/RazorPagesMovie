846beed1e62dc4a236c551f72d26f03fbb91d077
SudhaRatan <rattankumar2511@gmail.com>
2023-05-16 10:21:46 +0530

    2nd

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -8,6 +8,6 @@ public class Movie
     public string? Title { get; set; }
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string Genre { get; set; }
+    public string? Genre { get; set; }
     public decimal Price { get; set; }
 }
